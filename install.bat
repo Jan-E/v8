da1c1e0635aa489af9928c71953ad0d349d3c484
Jan-E <github@ehrhardt.nl>
2018-12-24 05:06:16 +0100

    Do not 'fetch v8' on Appveyor - time limit 59:59m

---
+++ install.bat
@@ -0,0 +1,17 @@
+C:
+cd C:\projects
+if exist depot_tools rd depot_tools /s /q
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+set PATH=C:\projects\depot_tools;%PATH%
+set GYP_MSVS_VERSION=2017
+set DEPOT_TOOLS_WIN_TOOLCHAIN=0
+set GYP_CHROMIUM_NO_ACTION=0
+cd C:\projects\depot_tools
+fetch V8
+cd C:\projects\depot_tools\v8
+call git fetch origin master
+call git checkout master
+call git pull
+call gclient sync
+cd C:\projects
+7z.exe a depot_tools.7z depot_tools
