78bc1b4c915919364d56d8bf0674975484f2be31
Jan-E <github@ehrhardt.nl>
2018-12-25 10:14:31 +0100

    Remove '& exit 0' from testing.bat [skip ci]

--- testing.bat
+++ testing.bat
@@ -10,10 +10,10 @@ set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set GYP_CHROMIUM_NO_ACTION=0
 cd C:\projects\depot_tools\v8\%v8_branch%\%v8_platform%.release
 rem failing in 7.1-lkgr (7.1.302.33)
-cctest.exe test-heap/TestSizeOfObjectsVsHeapIteratorPrecision --random-seed=975877910 --no-turbo-verify-allocation --stress-opt --always-opt --nohard-abort & exit 0
+cctest.exe test-heap/TestSizeOfObjectsVsHeapIteratorPrecision --random-seed=975877910 --no-turbo-verify-allocation --stress-opt --always-opt --nohard-abort
 rem failing in 7.1-lkgr and lkgr
-cctest.exe test-heap/MapRetaining --random-seed=975877910 --no-turbo-verify-allocation --stress-opt --always-opt --nohard-abort & exit 0
-inspector-test.exe ..\..\test\inspector\protocol-test.js ..\..\test\inspector\debugger/step-snapshot.js --random-seed=975877910 --no-turbo-verify-allocation --stress-opt --always-opt --nohard-abort --embed "function c(f, ...args) { return f(...args); }" & exit 0
-cctest.exe test-external-string-tracker/ExternalString_ExternalBackingStoreSizeIncreasesAfterExternalization --random-seed=975877910 --no-turbo-verify-allocation --stress-incremental-marking --nohard-abort & exit 0
+cctest.exe test-heap/MapRetaining --random-seed=975877910 --no-turbo-verify-allocation --stress-opt --always-opt --nohard-abort
+inspector-test.exe ..\..\test\inspector\protocol-test.js ..\..\test\inspector\debugger/step-snapshot.js --random-seed=975877910 --no-turbo-verify-allocation --stress-opt --always-opt --nohard-abort --embed "function c(f, ...args) { return f(...args); }"
+cctest.exe test-external-string-tracker/ExternalString_ExternalBackingStoreSizeIncreasesAfterExternalization --random-seed=975877910 --no-turbo-verify-allocation --stress-incremental-marking --nohard-abort
 cd C:\projects\depot_tools\v8
 call python tools/run-tests.py --progress=verbose --mode Release --outdir %v8_branch%\%v8_platform%.release --buildbot --timeout=200 --random-seed=975877910 bot_default --variants=more,dev --rerun-failures-count=2 --mastername client.v8 --buildername V8 Win64 - msvc --swarming > run-tests.txt
